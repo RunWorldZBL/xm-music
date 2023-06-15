fdd9915960913b3f4d7b3089858a9c7c2459603b
RunWorldZBL <runworldzbl@gmail.com>
2023-06-15 14:00:00 +0800

    final

--- push-with-date.sh
+++ push-with-date.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE="2023-06-15T14:00:00+0800"
 GIT_COMMITTER_DATE="$DATE" git commit --amend --date="$DATE" --no-edit
-git push --force-with-lease new-origin main
+git push --force-with-lease origin main
